aad8139a342f5601fbe0069936b7cb928b4b53ca
Prasad <prasadwadkar18@gmail.com>
2026-01-10 14:41:51 +0530

    Update ParticipantsTab.jsx

--- frontend/src/components/ParticipantsTab.jsx
+++ frontend/src/components/ParticipantsTab.jsx
@@ -8,7 +8,8 @@ function ParticipantsTab({
     onDeleteParticipant,
     onDeleteAllParticipants,
     onEditTemplate,
-    loading
+    loading,
+    isOwner
 }) {
     return (
         <div className="tab-content">
